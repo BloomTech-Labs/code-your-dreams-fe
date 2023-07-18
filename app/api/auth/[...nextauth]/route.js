928c3139378a8ea3a45e6f46f94702166d50addb
paulstgermain <paulg.stgermain@gmail.com>
2023-07-18 16:54:42 -0400

    adds logic in auth config to pull userRoles from Auth0 for future use

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,17 +5,29 @@ const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     Auth0Provider({
+      profile: function profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.nickname,
+          email: profile.email,
+          image: profile.picture,
+          roles: profile.userRoles
+        };
+      },
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       issuer: process.env.AUTH0_ISSUER_BASE_URL
     })
   ],
   callbacks: {
-    jwt: async function jwt({ token, account }) {
+    jwt: async function jwt({ token, account, user }) {
       if(account) {
         token.accessToken = account.access_token;
         token.idToken = account.id_token;
         token.id = token.sub;
+      }
+      if (user) {
+        token.roles = user.roles;
       }
 
       return token;
@@ -24,6 +36,7 @@ const authOptions = {
       session.accessToken = token.accessToken;
       session.idToken = token.idToken;
       session.user.id = token.id;
+      session.roles = token.roles;
 
       return session;
     }
