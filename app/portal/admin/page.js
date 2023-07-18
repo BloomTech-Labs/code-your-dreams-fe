b0097328b2b0360cddf57a0cf50bbe8f5aa75439
John Dodson <67118051+jpivvt@users.noreply.github.com>
2023-07-18 15:36:50 -0600

    admin and account page tweaks

--- app/portal/admin/page.js
+++ app/portal/admin/page.js
@@ -2,15 +2,12 @@
 
 import * as React from "react"
 import styles from "./page.module.scss"
-import { Button, IconButton } from "@mui/material"
-import AddIcon from "@mui/icons-material/Add"
 import { DataGrid } from "@mui/x-data-grid"
 
 const columns = [
   { field: "id", headerName: "ID", width: 100 },
   { field: "memberName", headerName: "Name", width: 250 },
   { field: "emailAddress", headerName: "Email", width: 300 },
-  { field: "chapterName", headerName: "Chapter", width: 300 },
   { field: "adminFlag", headerName: "Admin?", width: 150 },
 ]
 
@@ -19,43 +16,22 @@ const rows = [
     id: 1,
     memberName: "Brianne Caplan",
     emailAddress: "brianne@codeyourdreams.org",
-    chapterName: "CoderHeroes",
     adminFlag: "Yes",
   },
   {
     id: 2,
     memberName: "John Dodson",
-    emailAddress: "john.dodson@bloomtech.com",
-    chapterName: "BT Labs - Remote",
+    emailAddress: "john@codeyourdreams.org",
     adminFlag: "",
   },
 ]
 
 export default function Page() {
   return (
-    <main className={styles.main}>
+    <main className={styles.account}>
       <section className="container">
-        <div className="header-row">
-          <h1>Members</h1>
-          <div className="add-button">
-            <IconButton
-              color="primary"
-              size="large"
-              aria-label="add"
-              className="compact-button"
-            >
-              <AddIcon />
-            </IconButton>
-            <Button
-              variant="contained"
-              startIcon={<AddIcon />}
-              aria-label="add"
-              className="full-button"
-            >
-              Add a new member
-            </Button>
-          </div>
-        </div>
+        <h1>App Settings</h1>
+        <h2>Super admin users</h2>
         <div style={{ height: 500, width: "100%" }}>
           <DataGrid
             rows={rows}
@@ -68,6 +44,14 @@ export default function Page() {
             pageSizeOptions={[5, 10]}
           />
         </div>
+      </section>
+      <section className="container">
+        <h2>Materials types</h2>
+        <ul>
+          <li>Document</li>
+          <li>Presentation</li>
+          <li>etc.</li>
+        </ul>
       </section>
     </main>
   )
--- components/layout/Navigation/Navigation.jsx
+++ components/layout/Navigation/Navigation.jsx
@@ -1,14 +1,13 @@
 "use client"
 
 import styles from "./Navigation.module.scss"
-import { Button, IconButton } from "@mui/material"
-import HomeIcon from "@mui/icons-material/Home"
+import { Button } from "@mui/material"
 
 export default function Navigation() {
   return (
     <nav className={styles["site-nav"]}>
       <div className="container">
-        <span className="uppercase">Organization Name</span>
+        <span className="italic">Organization Name</span>
         {/* "Members" are restricted to Org Admins and higher */}
         <Button variant="text" href="/portal/members">
           Members
@@ -18,7 +17,7 @@ export default function Navigation() {
           Chapters
         </Button>
         {/* "Global Settings" is restricted to super admins */}
-        <Button variant="text" href="#">
+        <Button variant="text" href="/portal/admin">
           Global Settings
         </Button>
       </div>
