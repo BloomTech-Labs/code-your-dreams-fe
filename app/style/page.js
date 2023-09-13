f3203217152ca833bf09e88c8736fa4f4553058a
John Dodson <67118051+jpivvt@users.noreply.github.com>
2023-09-13 11:26:21 -1000

    added additional detail into styleguide page

--- app/style/page.js
+++ app/style/page.js
@@ -16,10 +16,6 @@ export default function Page() {
         </section>
         <section className="container">
           <h2>Typography</h2>
-          <p>
-            The app uses fluid typography for sizing for headers, based on a
-            base 1rem font size and the container width.
-          </p>
           <h3>Fonts</h3>
           <p>
             The site uses Google font{" "}
@@ -28,10 +24,22 @@ export default function Page() {
               target="_blank"
             >
               Roboto Flex
-            </a>
-            .
+            </a>{" "}
+            with a base size of 1rem.
           </p>
           <h3>Headings</h3>
+          <p>
+            The app uses fluid typography for sizing for headers, based on the
+            default font size and the container width. There are four header
+            types used in the app (
+            <code className={styles.highlighted}>h1</code> -{" "}
+            <code className={styles.highlighted}>h4</code>), as well as four
+            pseudo classes that can be used to apply header sizing to any text (
+            <code className={styles.highlighted}>".h1"</code>,{" "}
+            <code className={styles.highlighted}>".h2"</code>,{" "}
+            <code className={styles.highlighted}>".h3"</code>, and{" "}
+            <code className={styles.highlighted}>".h4"</code>).
+          </p>
           <div className={styles.examples}>
             <h1>Header 1</h1>
             <h2>Header 2</h2>
@@ -77,6 +85,21 @@ export default function Page() {
               Black: <code>black</code>
             </span>
           </div>
+        </section>
+        <section className="container">
+          <h2>UI Components</h2>
+          <p>
+            The application takes advantage of{" "}
+            <a href="https://mui.com/material-ui/" target="_new">
+              Material UI
+            </a>{" "}
+            components which use{" "}
+            <a href="https://material.io/" target="_new">
+              Material Design
+            </a>{" "}
+            styles by default. There is minimal diversion from the style library
+            applied in this app.
+          </p>
         </section>
       </main>
     </>
